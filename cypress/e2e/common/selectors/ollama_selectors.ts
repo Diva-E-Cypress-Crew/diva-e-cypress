10ca830183a017ce58c63265201f7d430ce2343f
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-05-09 22:18:01 +0200

    ollama's generated mess
    it is not working as a test yet but a good point to start

--- cypress/e2e/common/selectors/ollama_selectors.ts
+++ cypress/e2e/common/selectors/ollama_selectors.ts
@@ -1,3 +1,29 @@
-I can't see the .feature file you provided earlier. Could you please paste it here or describe the test scenario you want to write a Cypress test for? I'll help you with the cy selectors and the TypeScript code.
+import cy from 'cypress';
 
-Once I have this information, I'll provide the necessary cy selectors and the Cypress test code in TypeScript.
+cy.get('#form-container')
+  .should('be.visible')
+
+cy.get('.list > li')
+  .each((listItem) => {
+    cy.wrap(listItem).get('.anlage-plan-ueberpruefe').should('be.visible');
+    cy.wrap(listItem).get('.anlage-plan-ueberpruefe').click();
+    cy.wrap(listItem).get('.anlage-plan-wertentwicklung').should('be.visible');
+  });
+
+cy.get('.anlage-plan-wertentwicklung')
+  .each((item) => {
+    cy.wrap(item).get('#ausgabe-aufschlag').should('be.visible');
+    cy.wrap(item).get('#meag-fonds-gewinnanteil').should('be.visible');
+    cy.wrap(item).get('#meag-fonds-gewinnanteil > input').should('have.value', '0.1');
+    cy.wrap(item).get('#steuerliche-auswirkungen').should('be.visible');
+  });
+
+cy.get('.form-button')
+  .click();
+
+cy.get('.anlage-plan-ueberpruefe')
+  .each((listItem) => {
+    cy.wrap(listItem).get('#kosten-funktion').should('have.value', '1000.00');
+    cy.wrap(listItem).get('#verluste-funktion').should('be.visible');
+  });
+  
--- cypress/e2e/common/steps/ollama_steps.ts
+++ cypress/e2e/common/steps/ollama_steps.ts
@@ -1,7 +1,35 @@
-import { selector } from '../selectors/ollama_selectors';
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import olammaSelectors from '../selectors/ollama_selectors';
 
-export const steps = {
-  'Anlegen': (cy) => cy.get(selector.anlegenButton).click(),
-  'Löschen': (cy) => cy.get(selector.loeschButton).click(),
-  'Überprüfen': (cy) => cy.get(selector.ueberpruefeLabel).should('contain', 'Überprüfung erfolgreich'),
-};
+const olammaSelectors = new ollamaSelectors();
+
+Given('Anlegen Button ist sichtbar', () => {
+  cy.get(olammaSelectors.ANLEGEN_BUTTON).should('be.visible');
+});
+
+When('Anlageplan überprüfen', () => {
+  cy.get(olammaSelectors.LIST_ITEM).each((listItem) => {
+    cy.wrap(listItem).get(ollamaSelectors.ANLAGE_PLAN_UBERPRUEFE).should('be.visible');
+    cy.wrap(listItem).get(ollamaSelectors.ANLAGE_PLAN_UBERPRUEFE).click();
+    cy.wrap(listItem).get(ollamaSelectors.ANLAGE_PLAN_WERTENTWERKUNG).should('be.visible');
+  });
+});
+
+Then('Ausgabeaufschlag und Gewinnanteil sind sichtbar', () => {
+  cy.get(olammaSelectors.LIST_ITEM).each((listItem) => {
+    cy.wrap(listItem).get(ollamaSelectors.AUSGABEAUFSCLAG).should('be.visible');
+    cy.wrap(listItem).get(ollamaSelectors.MEAG_FONDS_GEWINNANTEIL).should('be.visible');
+    cy.wrap(listItem).get(ollamaSelectors.MEAG_FONDS_GEWINNANTEIL + ' > input').should('have.value', '0.1');
+  });
+});
+
+When('Form abschicken', () => {
+  cy.get(olammaSelectors.FORM_BUTTON).click();
+});
+
+Then('Verluste funktion ist sichtbar', () => {
+  cy.get(ollamaSelectors.LIST_ITEM).each((listItem) => {
+    cy.wrap(listItem).get(ollamaSelectors.KOSTEN_FUNKTION).should('have.value', '1000.00');
+    cy.wrap(listItem).get(ollamaSelectors.VERLIсте_FUNKTION).should('be.visible');
+  });
+});
