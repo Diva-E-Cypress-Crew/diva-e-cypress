4f6d7848eea959e8978463f67930aa376966eaa0
MaximilianBer <maximilianberthold@hotmail.com>
2025-05-13 16:56:03 +0200

    cypress Korrektur

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,39 +1,38 @@
-import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
-//@ts-ignore
-import browserify from '@badeball/cypress-cucumber-preprocessor/browserify'
-
-import { defineConfig } from 'cypress'
-const allureWriter = require('@shelex/cypress-allure-plugin/writer')
+const { defineConfig } = require('cypress');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const createBundler = require('@badeball/cypress-cucumber-preprocessor/browserify').default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const path = require('path');
 
 module.exports = defineConfig({
-    viewportHeight: 800,
-    viewportWidth: 1400,
-    watchForFileChanges: false,
-    defaultCommandTimeout: 15000,
-    trashAssetsBeforeRuns: true,
-    screenshotOnRunFailure: true,
-    video: false,
-    screenshotsFolder: './cypress/snapshots/actual',
-    e2e: {
-        async setupNodeEvents (on, config) {
-            await addCucumberPreprocessorPlugin(on, config)
-            await allureWriter(on, config)
+  viewportHeight: 800,
+  viewportWidth: 1400,
+  watchForFileChanges: false,
+  defaultCommandTimeout: 15000,
+  trashAssetsBeforeRuns: true,
+  screenshotOnRunFailure: true,
+  video: false,
+  screenshotsFolder: './cypress/snapshots/actual',
+  e2e: {
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      await allureWriter(on, config);
 
-            on(
-                'file:preprocessor',
-                browserify(config, {
-                    typescript: require.resolve('typescript')
-                })
-            )
+      on(
+        'file:preprocessor',
+        createBundler(config, {
+          typescript: require.resolve('typescript'),
+        })
+      );
 
-            return config
-        },
-        // baseUrl: 'https://ronnybetatester.4lima.de/',
-        baseUrl: 'https://meag.gitlab.diva-e.com/investmentrechner-2023',
-        specPattern: '**/*.{feature,features}',
+      return config;
     },
-    env: {
-        allure: true,
-        enableScreenshotDiffPlugin: true
-    }
-})
+    // baseUrl: 'https://ronnybetatester.4lima.de/',
+    baseUrl: 'https://meag.gitlab.diva-e.com/investmentrechner-2023',
+    specPattern: '**/*.{feature,features}',
+  },
+  env: {
+    allure: true,
+    enableScreenshotDiffPlugin: true,
+  },
+});
