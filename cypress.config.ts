4edce1e237e1f7a04e1110d2a585705e260633f0
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-05-13 19:05:26 +0200

    fix configs and edit package.json

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,29 @@
-const { defineConfig } = require('cypress');
-const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
-const createBundler = require('@badeball/cypress-cucumber-preprocessor/browserify').default;
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-const path = require('path');
+import { defineConfig } from 'cypress'
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+import allureWriter from '@shelex/cypress-allure-plugin/writer.js'
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild'
 
-module.exports = defineConfig({
+export default defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      allureWriter(on, config)
+
+      on('file:preprocessor', createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }))
+
+      return config
+    },
+    baseUrl: 'https://meag.gitlab.diva-e.com/investmentrechner-2023',
+    specPattern: 'cypress/e2e/**/*.feature',
+  },
+  env: {
+    allure: true,
+    enableScreenshotDiffPlugin: true
+  },
   viewportHeight: 800,
   viewportWidth: 1400,
   watchForFileChanges: false,
@@ -13,26 +32,4 @@ module.exports = defineConfig({
   screenshotOnRunFailure: true,
   video: false,
   screenshotsFolder: './cypress/snapshots/actual',
-  e2e: {
-    async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config);
-      await allureWriter(on, config);
-
-      on(
-        'file:preprocessor',
-        createBundler(config, {
-          typescript: require.resolve('typescript'),
-        })
-      );
-
-      return config;
-    },
-    // baseUrl: 'https://ronnybetatester.4lima.de/',
-    baseUrl: 'https://meag.gitlab.diva-e.com/investmentrechner-2023',
-    specPattern: '**/*.{feature,features}',
-  },
-  env: {
-    allure: true,
-    enableScreenshotDiffPlugin: true,
-  },
-});
+})
